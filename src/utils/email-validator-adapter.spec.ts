3e9208b41540700b5bb86c1931dac995f71e589f
tnncodes <tnncodes@gmail.com>
2022-08-12 19:07:43 -0300

    test: ensure EmailValidatorAdapter returns true if validator returns true

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,9 +1,24 @@
+import validator from 'validator';
+
 import { EmailValidatorAdapter } from './email-validator';
+
+jest.mock('validator', () => ({
+  isEmail(): boolean {
+    return true;
+  },
+}));
 
 describe('EmailValidatorAdapter', () => {
   test('should return false if validator returns false', () => {
     const sut = new EmailValidatorAdapter();
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false);
     const isValid = sut.isValid('invalid_email@email.com');
     expect(isValid).toBe(false);
+  });
+
+  test('should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter();
+    const isValid = sut.isValid('valid_email@email.com');
+    expect(isValid).toBe(true);
   });
 });
