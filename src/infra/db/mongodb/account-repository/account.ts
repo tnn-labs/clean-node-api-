0de525cc6d93318f61e359846168c66f10eb712a
tnncodes <tnncodes@gmail.com>
2022-08-23 12:00:13 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,18 @@
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository';
+import { AccountModel } from '../../../../domain/models/account';
+import { AddAccountModel } from '../../../../domain/usecases/add-account';
+import { MongoHelper } from '../helpers/mongo-helper';
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    // seleciona a coleção que deseja realizar operações
+    const accountCollection = MongoHelper.getCollection('accounts');
+
+    // insere os dados no banco
+    const result = await (await accountCollection).insertOne(accountData);
+
+    // retorna para o front com o id no formato sem o underline
+    const account = { ...accountData, id: result.insertedId.toHexString() };
+    return account;
+  }
+}
