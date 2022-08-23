695875e641de7b898d347292b136bcf185416014
tnncodes <tnncodes@gmail.com>
2022-08-23 15:26:02 -0300

    feat: ensure SignUp route returns an account on success

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,5 @@
 export default {
-  mongoUrl:
-    process.env.MONGO_URI || 'mongodb://localhost:27017/api-boilerplate',
+  mongoUrl: process.env.MONGO_URI || 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT || 3333,
   domain: process.env.DOMAIN || 'http://localhost',
 };
